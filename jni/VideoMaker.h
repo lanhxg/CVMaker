7931330940f193e7c1b58a25172917a833e965c4
lanhongxiang <lanhongxiang@xiaomi.com>
2015-04-24 10:44:49 +0800

    add file operation
    Signed-off-by: lanhongxiang <lanhongxiang@xiaomi.com>

--- jni/VideoMaker.h
+++ jni/VideoMaker.h
@@ -2,13 +2,34 @@
 #define VIDEO_MAKER_H__
 
 #include "internal/common.h"
+#include <vector>
+#include <string>
 
+using namespace std::string;
+using namespace std::vector;
 class VideoMaker {
-
+    
 public:
-    VideoMaker() {
-        LOGE("==lanhxg, hello world");
+    VideoMaker(): writer(NULL){ }
+
+    void addJPEGGFile(string file) {
+        m_fileList.push_back(file);
+    }
+
+    void createVideoWriter(string file, int fps, int w, int h, bool isColor) {
+        if (m_Writer) {
+            cvReleaseVideoWriter(&m_Writer);
+        }
     }
     
+
+private:
+    static void nativeAddJPEGFile(JNIEnv* env, jobject obj, jstring file) {
+    }
+    
+private:
+    typedef vector<string> FileList;
+    FileList m_fileList;
+    CvVideoWriter* m_writer;
 };
 #endif
